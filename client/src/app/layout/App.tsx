6f9e367cff079e848e5d3e429feda912b1a6f1b0
zobair.hu <zobair.hu@gmail.com>
2022-03-14 01:50:51 +0600

    90: creating a basket slice

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -17,16 +17,18 @@ import { getCookie } from "../util/util";
 import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../feature/checkout/CheckoutPage";
+import { useAppDispatch } from "../store/configureStore";
+import { setBasket } from "../../feature/basket/basketSlice";
 
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const buyerId = getCookie('BuyerId');
     if(buyerId) {
       agent.Basket.get()
-      .then(basket => setBasket(basket))
+      .then(basket => dispatch(setBasket(basket)))
       .catch(error => console.log(error))
       .finally(() => setLoading(false))
     } else{
