3342fa44eee70b9e3de11d40ad26b25d2c36e446
zobair.hu <zobair.hu@gmail.com>
2022-05-07 19:09:00 +0600

    156: Adding a custom text input

--- client/src/feature/checkout/AddressForm.tsx
+++ client/src/feature/checkout/AddressForm.tsx
@@ -1,96 +1,36 @@
-import { Typography, Grid, TextField, FormControlLabel, Checkbox } from "@mui/material";
+import { Typography, Grid, TextField, FormControlLabel, Checkbox, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
+import AppTextInput from "../../app/components/AppTextInput";
 
 export default function AddressForm() {
-    return (
-      <>
-        <Typography variant="h6" gutterBottom>
-          Shipping address
-        </Typography>
+  const {control, handleSubmit} = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="firstName"
-              name="firstName"
-              label="First name"
-              fullWidth
-              autoComplete="given-name"
-              variant="standard"
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="lastName"
-              name="lastName"
-              label="Last name"
-              fullWidth
-              autoComplete="family-name"
-              variant="standard"
-            />
+          <Grid item xs={12} sm={12}>
+            <AppTextInput control={control} name='fullName' label='Full name' />
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              required
-              id="address1"
-              name="address1"
-              label="Address line 1"
-              fullWidth
-              autoComplete="shipping address-line1"
-              variant="standard"
-            />
+            <AppTextInput control={control} name='address1' label='Address 1' />
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              id="address2"
-              name="address2"
-              label="Address line 2"
-              fullWidth
-              autoComplete="shipping address-line2"
-              variant="standard"
-            />
+            <AppTextInput control={control} name='address2' label='Address 2' />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="city"
-              name="city"
-              label="City"
-              fullWidth
-              autoComplete="shipping address-level2"
-              variant="standard"
-            />
+            <AppTextInput control={control} name='city' label='City' />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              id="state"
-              name="state"
-              label="State/Province/Region"
-              fullWidth
-              variant="standard"
-            />
+            <AppTextInput control={control} name='state' label='State' />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="zip"
-              name="zip"
-              label="Zip / Postal code"
-              fullWidth
-              autoComplete="shipping postal-code"
-              variant="standard"
-            />
+            <AppTextInput control={control} name='zip' label='Zipcode' />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              id="country"
-              name="country"
-              label="Country"
-              fullWidth
-              autoComplete="shipping country"
-              variant="standard"
-            />
+            <AppTextInput control={control} name='country' label='Country' />
           </Grid>
           <Grid item xs={12}>
             <FormControlLabel
@@ -99,7 +39,9 @@ export default function AddressForm() {
             />
           </Grid>
         </Grid>
-      </>
-    );
+        <Button type='submit'>Submit Form</Button>
+      </form>
+    </>
+  );
   }
   
