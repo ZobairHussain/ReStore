8d37bd152b9db8232e5d0cce99012000c9b8c78c
zobair.hu <zobair.hu@gmail.com>
2022-05-11 11:20:41 +0600

    162: Submitting the order

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { request } from "http";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { PaginatedResopnse } from "../models/pagination";
@@ -93,11 +94,18 @@ const Account = {
     currentUser: () => requests.get('account/currentUser'),
 }
 
+const Orders = {
+    list: () => requests.get('orders'),
+    fetch: (id: number) => requests.get(`orders/${id}`),
+    create: (values: any) => requests.post('orders', values)
+}
+
 const agent = {
     Catalog,
     TestErrors,
     Basket,
-    Account
+    Account,
+    Orders
 }
 
 export default agent;
