1f4ecda718f6886f4afd72dbb5ccb9651778eb6d
zobair.hu <zobair.hu@gmail.com>
2022-05-16 18:16:31 +0600

    177: Adding the payment intent function

--- client/src/feature/checkout/CheckoutWrapper.tsx
+++ client/src/feature/checkout/CheckoutWrapper.tsx
@@ -1,10 +1,27 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
 import CheckoutPage from "./CheckoutPage";
 
 const stripePromise = loadStripe('pk_test_51KygoqJces2I16yjqNbsrq87foPioBydZo6Bw6PS2GOCGMkmYsIW0oZ9Xcenp2NwlCNJibEsbhqm40UqIr0vBi7i00tH6vS9Cj')
 
 export default function CheckoutWrapper() {
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+        .then(basket => dispatch(setBasket(basket)))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    }, [dispatch])
+
+    if(loading) <LoadingComponent message='Loading checkout...' />
+
     return (
         <Elements stripe={stripePromise}>
             <CheckoutPage />
