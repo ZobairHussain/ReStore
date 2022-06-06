286ef32e49f9e81e8d470e09a8f7733bdfa2035a
zobair.hu <zobair.hu@gmail.com>
2022-06-06 15:37:12 +0600

    200: updating & deleting image

--- API/Controllers/productsController.cs
+++ API/Controllers/productsController.cs
@@ -85,15 +85,30 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
         {
             var product  = await _context.Products.FindAsync(productDto.Id);
             if(product == null) return NotFound();
 
             _mapper.Map(productDto, product);
+
+            if(productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if(imageResult.Error != null) 
+                    return BadRequest(new ProblemDetails{Title = imageResult.Error.Message});
+
+                if(!string.IsNullOrEmpty(product.PublicId))
+                    await _imageService.DeleteImageAsync(product.PublicId);
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.PublicId = imageResult.PublicId;
+            }
+
             var result = await _context.SaveChangesAsync() > 0;
 
-            if(result) return NoContent();
+            if(result) return Ok(product);
 
             return BadRequest(new ProblemDetails{Title = "Problem updating product"});
         }
@@ -105,6 +120,9 @@ namespace API.Controllers
              var product  = await _context.Products.FindAsync(id);
             if(product == null) return NotFound();
 
+            if(!string.IsNullOrEmpty(product.PublicId))
+                await _imageService.DeleteImageAsync(product.PublicId);
+            
             _context.Products.Remove(product);
 
             var result = await _context.SaveChangesAsync() > 0;
