619b78678b5f20a6aed9485afa2c3db248f7a945
zobair.hu <zobair.hu@gmail.com>
2022-06-08 09:10:44 +0600

    build fix

--- client/src/app/layout/header.tsx
+++ client/src/app/layout/header.tsx
@@ -5,24 +5,24 @@ import { Link, NavLink } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
 import SignedInManu from "./SignedInManu";
 
-interface Props{
+interface Props {
     darkMode: boolean;
     handleThemeChanged: () => void;
 }
 
 const midLinks = [
-    {title: 'catalog', path: '/catalog'},
-    {title: 'about', path: '/about'},
-    {title: 'contact', path: '/contact'}
+    { title: 'catalog', path: '/catalog' },
+    { title: 'about', path: '/about' },
+    { title: 'contact', path: '/contact' }
 ]
 
 const rightLinks = [
-    {title: 'login', path: '/login'},
-    {title: 'register', path: '/register'},
+    { title: 'login', path: '/login' },
+    { title: 'register', path: '/register' },
 ]
 
 const navStyles = {
-    color: 'inherit', 
+    color: 'inherit',
     textDecoration: 'none',
     typography: 'h6',
     '&:hover': {
@@ -32,26 +32,26 @@ const navStyles = {
         color: 'text.secondary'
     }
 }
-export default function Header({darkMode, handleThemeChanged}: Props) {
-    const {basket} = useAppSelector(state => state.basket);
-    const {user} = useAppSelector(state => state.account);
+export default function Header({ darkMode, handleThemeChanged }: Props) {
+    const { basket } = useAppSelector(state => state.basket);
+    const { user } = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0); // reduce() Calls the specified callback function for all the elements in an array
 
-    return(
+    return (
         <AppBar position='static'>
-            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Box display='flex' alignItems='center'>
-                    <Typography variant='h6' 
-                    exact
-                    component={NavLink} to='/' 
-                    sx={navStyles}>
+                    <Typography variant='h6'
+                        exact
+                        component={NavLink} to='/'
+                        sx={navStyles}>
                         RE-STORE
                     </Typography>
                     <Switch checked={darkMode} onChange={handleThemeChanged} />
                 </Box>
-                
-                <List sx={{display: 'flex'}}>
-                    {midLinks.map(({title, path}) => (
+
+                <List sx={{ display: 'flex' }}>
+                    {midLinks.map(({ title, path }) => (
                         <ListItem
                             component={NavLink}
                             to={path}
@@ -62,18 +62,18 @@ export default function Header({darkMode, handleThemeChanged}: Props) {
                         </ListItem>
                     ))}
                     {user && user.roles?.includes('Admin') &&
-                    <ListItem
+                        <ListItem
                             component={NavLink}
                             to={'/inventory'}
                             sx={navStyles}
                         >
 
                             INVENTORY
-                        </ListItem> }
+                        </ListItem>}
                 </List>
 
                 <Box display='flex' alignItems='center'>
-                    <IconButton component={Link} to='/basket' size='large' sx={{color: 'inherit'}}>
+                    <IconButton component={Link} to='/basket' size='large' sx={{ color: 'inherit' }}>
                         <Badge badgeContent={itemCount} color='secondary'>
                             <ShoppingCart />
                         </Badge>
@@ -81,8 +81,8 @@ export default function Header({darkMode, handleThemeChanged}: Props) {
                     {user ? (
                         <SignedInManu />
                     ) : (
-                        <List sx={{display: 'flex'}}>
-                            {rightLinks.map(({title, path}) => (
+                        <List sx={{ display: 'flex' }}>
+                            {rightLinks.map(({ title, path }) => (
                                 <ListItem
                                     component={NavLink}
                                     to={path}
@@ -94,9 +94,9 @@ export default function Header({darkMode, handleThemeChanged}: Props) {
                             ))}
                         </List>
                     )}
-                    
+
                 </Box>
-                
+
             </Toolbar>
         </AppBar>
     )
